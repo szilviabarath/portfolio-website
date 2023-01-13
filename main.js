dc6771498ccc74edcd1183b1851d21ea9a3871cf
szilviabarath <szilvia.barat@gmail.com>
2023-01-13 11:32:29 +0100

    site

---
+++ main.js
@@ -0,0 +1,33 @@
+//window.onscroll = function() {scrollFunction()};
+
+//function scrollFunction() {
+  //if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    //document.getElementById("navbar").style.top = "0";
+  //} else {
+    //document.getElementById("navbar").style.top = "-50px";
+  //}
+//}//
+
+$(function () {
+  $('button').clickFireworks({
+
+    // canvas id
+    id: 'fireworks',
+
+    // append canvas to where, default is body
+    appendTo: 'body',
+
+    // canvas z-index, make it higher than anything on the page
+    zIndex: 1000
+
+  });
+});
+
+const myTags = [
+  'JavaScript', 'CSS', 'HTML',
+  'Python', 'git',
+  'Bootstrap', 'Node.js', 'Tailwind CSS',
+   'MySQL', 'jQuery', 'Photoshop', 'Blender',
+];
+
+var tagCloud = TagCloud('.content', myTags);
